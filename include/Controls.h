4e4bec52392997850532763af6e5d72bc712d01d
BuildTools <unconfigured@null.spigotmc.org>
2021-04-11 17:32:19 -0400

    Guidance

--- include/Controls.h
+++ include/Controls.h
@@ -4,8 +4,9 @@
 #include <glm/glm.hpp>
 using glm::vec3;
 using glm::mat3;
+using glm::vec4;
 
 namespace adcs::controls {
-
+    void Guidance(int mode,vec3 se,vec3 pe,vec3 ve,vec3 ne,vec3 ae,vec3 ge,vec3 %pointingE,vec3 %constraintE,vec3 %pointingB,vec3 %constraintB);
 
 }
